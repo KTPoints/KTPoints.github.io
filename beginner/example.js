820f2a4f9c4c71412459e1a0bf20e79fe10f7fac
sletap <sletap@umich.edu>
2019-05-26 08:50:59 -0500

    Add beginner to Vue/Bulma tutorial

---
+++ beginner/example.js
@@ -0,0 +1,29 @@
+// instantiate Vue 
+let example = new Vue({
+  // Where to attach
+  // Only things within example can be changed 
+  // # applies to IDs
+  el: "#example",
+
+  // variables
+  data: {
+    input: '',
+    text: '',
+    isCenter: false,
+  },
+
+  // functions
+  methods: {
+    changeText: function(){
+      // use this to access our data
+      this.text = "My Name Is " + this.input;
+    },
+
+    centerText: function(){
+      this.isCenter = !this.isCenter;
+    }
+  },
+
+  // There are a TON of other things too on top of el, data, and methods (such as computed, mounted, watch etc.)
+  // The Vue documentation is pretty good at providing examples for all these things
+})
